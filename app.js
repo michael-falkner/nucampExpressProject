60ae640db9faa372edde9806e49e99541f33c825
Michael Falkner <michael.allen.falkner@gmail.com>
2021-05-29 13:18:04 -0400

    Passport

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var logger = require('morgan');
 
@@ -47,23 +48,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-function auth(req, res, next) {
-  console.log(req.session);
 
-  if (!req.session.user) {
-      const err = new Error('You are not authenticated!');
+function auth(req, res, next) {
+  console.log(req.user);
+  console.log("Testing");
+
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');                    
       err.status = 401;
       return next(err);
   } else {
-      if (req.session.user === 'authenticated') {
-          return next();
-      } else {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
-      }
+      return next();
   }
 }
 
